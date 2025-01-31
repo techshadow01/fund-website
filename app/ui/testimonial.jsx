04f597f0319e02c5a92776414fe0340adaa2967a
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-01-31 16:20:08 +0530

    Add files via upload

--- app/ui/testimonial.jsx
+++ app/ui/testimonial.jsx
@@ -34,8 +34,8 @@ const AnimatedTestimonials = ({
     };
     return (
         (<div
-            className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
-            <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
+            className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 lg:px-12 py-20 max-sm:py-10">
+            <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20 max-sm:gap-5">
                 <div>
                     <div className="relative h-80 w-full">
                         <AnimatePresence>
@@ -68,7 +68,7 @@ const AnimatedTestimonials = ({
                                         duration: 0.4,
                                         ease: "easeInOut",
                                     }}
-                                    className="absolute inset-0 origin-bottom">
+                                    className="absolute inset-0 origin-bottom ">
                                     <Image
                                         src={testimonial.src}
                                         alt={testimonial.name}
@@ -100,16 +100,16 @@ const AnimatedTestimonials = ({
                             duration: 0.2,
                             ease: "easeInOut",
                         }}>
-                        <h3 className="text-4xl font-bold text-black">
+                        <h3 className="text-4xl font-bold text-black dark:text-white">
                             {testimonials[active].name}
                         </h3>
                         <p className=" text-gray-400">
                             {testimonials[active].designation}
                         </p>
-                        <motion.p className="text-lg text-black mt-8">
-                            {testimonials[active].quote.split(" ").map((word, index) => (
+                        <motion.p className="text-lg text-black dark:text-white mt-8">
+                            {testimonials[active].quote.split(" ").map((word, index1) => (
                                 <motion.span
-                                    key={index}
+                                    key={index1}
                                     initial={{
                                         filter: "blur(10px)",
                                         opacity: 0,
@@ -123,7 +123,7 @@ const AnimatedTestimonials = ({
                                     transition={{
                                         duration: 0.2,
                                         ease: "easeInOut",
-                                        delay: 0.02 * index,
+                                        delay: 0.02 * index1,
                                     }}
                                     className="inline-block">
                                     {word}&nbsp;
@@ -134,13 +134,13 @@ const AnimatedTestimonials = ({
                     <div className="flex gap-4 pt-12 md:pt-0">
                         <button
                             onClick={handlePrev}
-                            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
+                            className="max-sm:hidden h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
                             <IconArrowLeft
                                 className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300" />
                         </button>
                         <button
                             onClick={handleNext}
-                            className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
+                            className="max-sm:hidden h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
                             <IconArrowRight
                                 className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300" />
                         </button>
