abf15b5822663d12f7063b0bbd77cf640230ddcb
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-01-31 15:42:54 +0530

    Update testimonial.jsx

--- app/ui/testimonial.jsx
+++ app/ui/testimonial.jsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import { IconArrowLeft, IconArrowRight } from "@tabler/icons-react";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
